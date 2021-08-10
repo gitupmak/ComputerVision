699333bc1de5fe02a7adc01d653a0efe3341f09e
gitupmak <aslamece1097@gmail.com>
2021-08-10 19:24:12 +0530

    corrected VideoCapture

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,10 +1,9 @@
-# Aslam Khan
 import cv2
+cap = cv2.VideoCapture(0)
 while True:
-    cap = cv2.VideoCapture(0)
     check,frame = cap.read()
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) == ord('q'):
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
