6fa9d514c403933b48e4a7c35da19b66a973c321
gitupmak <aslamece1097@gmail.com>
2021-08-10 19:33:23 +0530

    flip the video for mirror effect

--- VideoCapture.py
+++ VideoCapture.py
@@ -2,7 +2,9 @@ import cv2
 cap = cv2.VideoCapture(0)
 while True:
     check,frame = cap.read()
-    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.flip(gray, 1)
+    cv2.imshow('gray', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
